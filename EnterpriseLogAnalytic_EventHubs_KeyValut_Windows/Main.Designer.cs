b73e5f48a701569f07234da1bdfd5ddbb7d38a57
Phupaa <phupaa@hotmail.com>
2018-06-13 13:37:02 +0700

    Version 1.0

---
+++ EnterpriseLogAnalytic_EventHubs_KeyValut_Windows/Main.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace EnterpriseLogAnalytic_EventHubs_KeyValut_Windows
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Output1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1070, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(430, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1.Login to ADAL and Get Secret";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Output1
+            // 
+            this.Output1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.Output1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Output1.Location = new System.Drawing.Point(130, 63);
+            this.Output1.Margin = new System.Windows.Forms.Padding(6);
+            this.Output1.Name = "Output1";
+            this.Output1.Size = new System.Drawing.Size(806, 1002);
+            this.Output1.TabIndex = 1;
+            this.Output1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(1072, 675);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(430, 77);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Send event to EventHubs";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1094, 346);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 37);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "50";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1066, 310);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "2.Enter number of event to be sent";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(732, 1071);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Phupaa | June 2018";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1756, 1137);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Output1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MinimumSize = new System.Drawing.Size(1782, 1208);
+            this.Name = "Main";
+            this.Text = "Main form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox Output1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+
+        // Version 2: will implement later
+        /*
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        */
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EnterpriseLogAnalytic_EventHubs_KeyValut_Windows/Main.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+
+namespace EnterpriseLogAnalytic_EventHubs_KeyValut_Windows
+{
+    public partial class Main : Form
+    {
+
+        //
+        // Sample code to demonstrate how to logon to Azure AD with AppID and access to Azure Key Vault 
+        // to get secret (SAS TOKEN) to access and send data to Azure Event Hubs. 
+        //
+
+        delegate void StringArgReturningVoidDelegate(string text);
+        // Please consider if you want to hardcode this ClientID and Secret?        
+        public static string ClientID = "REPLACE THIS WITH YOUR CLIENT/APP ID HERE";
+        public static string ClientSecret = "REPLACE THIS WITH YOUR CLIENT/APP PASSWORD";
+        public static string SecretName= "REPLACE THIS WITH YOUR SECRET NAME IN KEY VAULT";
+        public static string AzureVaultURI = "REPLACE THIS WITH YOUR KEY VAULT URI";
+        public static EventHubClient eventHubClient;
+        public static string EhConnectionString = "";
+        public const string EhEntityPath = "REPLACE THIS WITH EVENT HUBS ENTITY NAME";
+
+
+        //recommend reading: 
+        // How to: Make Thread-Safe Calls to Windows Forms Controls
+        // https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/how-to-make-thread-safe-calls-to-windows-forms-controls 
+
+        private void WriteOutput(string text)
+        {
+            if (this.Output1.InvokeRequired)
+            {
+                StringArgReturningVoidDelegate d = new StringArgReturningVoidDelegate(WriteOutput);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                Output1.Text = Output1.Text + text + "\n---------------\n";
+                
+                Output1.SelectionStart = Output1.Text.Length;
+                Output1.ScrollToCaret();
+            }
+            
+        }
+        private async void authUsingADALCallbackAsync() 
+        {
+            WriteOutput("Authenticating to Key Vault using ADAL callback.");
+            WriteOutput(AzureVaultURI);
+
+
+            // User ClientID and Password to login to AzureAD
+            // then create connection to KeyVault 
+            KeyVaultClient kvClient = new KeyVaultClient(
+                async (string authority, string resource, string scope) =>
+                {
+                    var authContext = new AuthenticationContext(authority);
+                    ClientCredential clientCred = new ClientCredential(ClientID, ClientSecret);
+                    AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+                    if (result == null)
+                    {
+                        throw new InvalidOperationException("Failed to retrieve access token for Key Vault, perhaps you DO NOT have authorization to access.  ");
+
+                    }
+                    WriteOutput("Login succeed! I got the token from ADAL!"+ "\n");
+                    //WriteOutput(result.AccessToken);
+                    return result.AccessToken;
+                }
+            );
+
+            // After get KeyVault Connection, get the secret. 
+            SecretBundle s = await kvClient.GetSecretAsync(AzureVaultURI, SecretName);
+            EhConnectionString = s.Value;            
+            WriteOutput(SecretName + " is " + EhConnectionString);
+        }
+        
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WriteOutput("Hello world of ADAL and KV!!");
+            authUsingADALCallbackAsync();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+        private static string RandomString(int size)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+
+        }
+        //private async Task SendMessagesToEventHub(int numMessagesToSend)
+        private async void SendMessagesToEventHub(int numMessagesToSend)
+        {
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EhConnectionString)
+            {
+                EntityPath = EhEntityPath
+            };
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+              
+            
+            Random rand = new Random();
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+
+                    var telemetryDataPoint = new
+                    {
+                        deviceId = RandomString(3),
+                        menuName = RandomString(1),
+                        Message = RandomString(2),
+                        Severity = rand.Next(1, 4)
+                    };
+                    var messageString = JsonConvert.SerializeObject(telemetryDataPoint);                    
+                    var message = Encoding.UTF8.GetBytes(messageString);
+                    WriteOutput($"Sending message #: {i}");
+                    await eventHubClient.SendAsync(new EventData(message));
+                }
+                catch (Exception exception)
+                {
+                    WriteOutput($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            WriteOutput($"{numMessagesToSend} messages sent.");
+
+            await eventHubClient.CloseAsync();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WriteOutput("Hello world of Event Hubs!!");
+            SendMessagesToEventHub(Convert.ToInt32(textBox1.Text));
+
+
+        }
+    }
+}
